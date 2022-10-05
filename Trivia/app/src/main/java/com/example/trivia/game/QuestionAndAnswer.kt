e74c940b3c6b20777598c7d7bedc94e0c2b3379c
manish <43578519+manish8621@users.noreply.github.com>
2022-10-05 03:07:20 +0000

    Delete Trivia/app directory

--- Trivia/app/src/main/java/com/example/trivia/game/QuestionAndAnswer.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trivia.game
-
-class QuestionAndAnswer(val question:String,val choices:Array<String>,val answer:String,var answerToCheck:String=""){
-    val isRightAnswerGiven:Boolean
-        get() = answer == answerToCheck
-}
